256b4c42b8d4a078076a3f6a4a3c586dd602651c
faizah1992 <faizah1992@gmail.com>
2020-09-23 16:04:40 -0500

    like button added

---
+++ src/components/common/like.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Like extends Component {
+    
+    render() { 
+        return (
+            <i class='fa fa-heart-0' aria-hidden='true'></i>
+        );
+    }
+}
+ 
+export default Like;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService'
+import Like from "./common/like"
 
 class Movies extends Component {
     state = { 
@@ -8,10 +9,20 @@ class Movies extends Component {
 
     handeDelete = (movie) => {
         const movies = this.state.movies.filter(m => m._id !== movie._id)
-        this.setState({ movies})
+        this.setState({ movies })
+    }
+
+    handleLike = () => {
+
     }
     render() { 
+        // const { length: count } = this.state.movies
+        if (this.state.movies.length === 0)
+        return <p>there are no movies in the database.</p>
+
         return ( 
+            <div>
+           <p>Showing {this.state.movies.length} movies in the database.</p>
            <table className="table">
                <thead>
                    <tr>
@@ -19,6 +30,7 @@ class Movies extends Component {
                        <th>Genre</th>
                        <th>Stock</th>
                        <th>Rate</th>
+                       <th></th>
                        <th></th>
                    </tr>
                </thead>
@@ -29,11 +41,13 @@ class Movies extends Component {
                             <td>{movie.genre.name}</td>
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
+                            <td><Like/></td>
                             <td><button onClick= {() => this.handeDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                         </tr>
                    ))}
                </tbody>
            </table>
+           </div>
         );
     }
 }
