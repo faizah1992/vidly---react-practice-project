3c90750eb52f28bbfa3fb99eac35dc937c98a661
faizah1992 <faizah1992@gmail.com>
2020-09-30 09:21:05 -0500

    added handlelogin

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -12,19 +12,21 @@ class LoginForm extends Component {
  
     }
 
-    handleChange = e => {
+    handleChange = ({currentTarget: input}) => {
         const account = {...this.state.account}
-        account.username = e.currentTarget.value
+        account[input.name] = input.value
         this.setState({ account })
     }
   
     render() { 
+        const { account }=this.state
         return (  
             <div>
                 <h1>Login</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <div className="form-group"><label htmlFor="username">Username</label><input value={this.state.account.username} onChange={this.handleChange} id="username" type="text" className="form-control"/></div>
-                    <div className="form-group"><label htmlFor="password">Password</label><input id='password' type="text" className="form-control"/></div>
+                    <div className="form-group"><label htmlFor="username">Username</label><input value={account.username} onChange={this.handleChange} id="username" name= 'username' type="text" className="form-control"/></div>
+                    <div className="form-group"><label htmlFor="password">Password</label><input value=
+                    {account.password} onChange={this.handleChange} name="password"  id='password' type="text" className="form-control"/></div>
                     <button className="btn btn-primary">Login</button>
                 </form>
             </div>
