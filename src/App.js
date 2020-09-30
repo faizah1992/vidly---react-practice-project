758d981fe2f77692d8aa9bfd60e2b997f8b37e10
faizah1992 <faizah1992@gmail.com>
2020-09-30 09:14:33 -0500

    added login button

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NavBar from './components/navBar';
 import Customers from './components/customers'
 import Rentals from './components/rentals'
 import NotFound from './components/notFound'
-
+import LoginForm from './components/loginForm'
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
       {/* <Movies/> */}
       <NavBar />
       <Switch>
+        <Route path="/login" component={LoginForm}/>
         <Route path="/movies/:id" component={MovieForm}/>
         <Route path="/movies" component={Movies}></Route>
         <Route path="/customers" component={Customers}></Route>
